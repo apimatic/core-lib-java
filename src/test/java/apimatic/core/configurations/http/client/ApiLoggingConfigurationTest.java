c5decc335574605409ab51ee72557abc2b706b98
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2024-04-17 10:56:21 +0500

    remove unused imports

--- src/test/java/apimatic/core/configurations/http/client/ApiLoggingConfigurationTest.java
+++ src/test/java/apimatic/core/configurations/http/client/ApiLoggingConfigurationTest.java
@@ -1,13 +1,10 @@
 package apimatic.core.configurations.http.client;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import java.util.TreeSet;
 import org.junit.Test;
 import io.apimatic.core.configurations.http.client.ApiLoggingConfiguration;
 import io.apimatic.coreinterfaces.http.LoggingLevel;
-import io.apimatic.coreinterfaces.http.LoggingPolicy;
 
 public class ApiLoggingConfigurationTest {
     @Test
