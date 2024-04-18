c092f1b245bb496c88f0cdf3f08f07a8b89f687a
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2024-04-18 18:58:28 +0500

    change constants interface to final class

--- src/main/java/io/apimatic/core/logger/LoggerConstants.java
+++ src/main/java/io/apimatic/core/logger/LoggerConstants.java
@@ -1,47 +1,49 @@
 package io.apimatic.core.logger;
 
 /**
- * Interface defining constants related to logging. These constants are used for
+ * Class defining constants related to logging. These constants are used for
  * logging various aspects of HTTP requests and responses.
  */
-public interface LoggerConstants {
+public final class LoggerConstants {
     /**
      * Key representing the method of the HTTP request.
      */
-    String METHOD = "method";
+    public static final String METHOD = "method";
 
     /**
      * Key representing the URL of the HTTP request.
      */
-    String URL = "url";
+    public static final String URL = "url";
 
     /**
      * Key representing the content type of the HTTP request or response.
      */
-    String CONTENT_TYPE = "content-type";
+    public static final String CONTENT_TYPE = "content-type";
 
     /**
      * Key representing the query parameters of the HTTP request.
      */
-    String QUERY_PARAMETER = "queryParameter";
+    public static final String QUERY_PARAMETER = "queryParameter";
 
     /**
      * Key representing the headers of the HTTP request or response.
      */
-    String HEADERS = "headers";
+    public static final String HEADERS = "headers";
 
     /**
      * Key representing the body of the HTTP request or response.
      */
-    String BODY = "body";
+    public static final String BODY = "body";
 
     /**
      * Key representing the content length of the HTTP request or response.
      */
-    String CONTENT_LENGTH = "content-length";
+    public static final String CONTENT_LENGTH = "content-length";
 
     /**
      * Key representing the status code of the HTTP response.
      */
-    String STATUS_CODE = "statuscode";
+    public static final String STATUS_CODE = "statuscode";
+
+    private LoggerConstants() {}
 }
