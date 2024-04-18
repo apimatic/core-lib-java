b847e845c22fcc05ce2c20b4c8b7fc16933e168d
hamzamahmood <hamzamehmood18@gmail.com>
2024-04-18 10:56:22 +0500

    confilct resolution

--- src/main/java/io/apimatic/core/logger/SdkLogger.java
+++ src/main/java/io/apimatic/core/logger/SdkLogger.java
@@ -1,8 +1,5 @@
 package io.apimatic.core.logger;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.event.Level;
@@ -29,12 +26,6 @@ public class SdkLogger implements ApiLogger {
 	private ReadonlyLogging config;
 
 	/**
-	 * List of sensitive headers that need to be filtered.
-	 */
-	private static final List<String> sensitiveHeaders = Arrays.asList("authorization", "www-authenticate",
-			"proxy-authorization", "set-cookie");
-
-	/**
 	 * Default Constructor.
 	 * 
 	 * @param config {@link ReadonlyLogging} as logging properties.
@@ -58,7 +49,7 @@ public class SdkLogger implements ApiLogger {
 				? request.getHeaders().value("content-type")
 				: "";
 
-		if (config.getRequestLogOptions().shouldIncludeQueryInPath() && !queryParameter.isEmpty()) {
+		if (config.getRequestLogOptions().shouldIncludeQueryInPath()) {
 			logger.log(level, "Request {} {} {} queryParameters: {}", request.getHttpMethod(), url, contentType,
 					queryParameter);
 		} else {
@@ -66,11 +57,11 @@ public class SdkLogger implements ApiLogger {
 		}
 
 		if (config.getRequestLogOptions().shouldLogHeaders()) {
-			Map<String, String> extractedHeaders = extractHeadersToLog(request.getHeaders().asSimpleMap(),
-					config.getRequestLogOptions().getHeadersToInclude(),
+			Map<String, String> extractedHeaders = LoggerUtilities.extractHeadersToLog(
+					request.getHeaders().asSimpleMap(), config.getRequestLogOptions().getHeadersToInclude(),
 					config.getRequestLogOptions().getHeadersToExclude());
 
-			Map<String, String> headersToLog = filterSensitiveHeaders(extractedHeaders,
+			Map<String, String> headersToLog = LoggerUtilities.filterSensitiveHeaders(extractedHeaders,
 					config.getMaskSensitiveHeaders());
 
 			logger.log(level, "Request Headers {}", headersToLog);
@@ -98,19 +89,14 @@ public class SdkLogger implements ApiLogger {
 				? response.getHeaders().value("content-type")
 				: "";
 
-		if (contentLength == null) {
-			logger.log(level, "Response {} {}", response.getStatusCode(), contentType);
-		} else {
-			logger.log(level, "Response {} {} content-length: {}", response.getStatusCode(), contentType,
-					contentLength);
-		}
+		logger.log(level, "Response {} {} content-length: {}", response.getStatusCode(), contentType, contentLength);
 
 		if (config.getResponseLogOptions().shouldLogHeaders()) {
-			Map<String, String> extractedHeaders = extractHeadersToLog(response.getHeaders().asSimpleMap(),
-					config.getResponseLogOptions().getHeadersToInclude(),
+			Map<String, String> extractedHeaders = LoggerUtilities.extractHeadersToLog(
+					response.getHeaders().asSimpleMap(), config.getResponseLogOptions().getHeadersToInclude(),
 					config.getResponseLogOptions().getHeadersToExclude());
 
-			Map<String, String> headersToLog = filterSensitiveHeaders(extractedHeaders,
+			Map<String, String> headersToLog = LoggerUtilities.filterSensitiveHeaders(extractedHeaders,
 					config.getMaskSensitiveHeaders());
 
 			logger.log(level, "Response Headers {}", headersToLog);
@@ -119,85 +105,5 @@ public class SdkLogger implements ApiLogger {
 		if (config.getResponseLogOptions().shouldLogBody()) {
 			logger.log(level, "Response Body {}", response.getBody());
 		}
-	}
-
-	/**
-	 * Extracts headers to log based on inclusion and exclusion criteria.
-	 * 
-	 * @param headers          The map of headers.
-	 * @param headersToInclude The set of headers to include.
-	 * @param headersToExclude The set of headers to exclude.
-	 * @return The extracted headers to log.
-	 */
-	public Map<String, String> extractHeadersToLog(Map<String, String> headers, List<String> headersToInclude,
-			List<String> headersToExclude) {
-		if (!headersToInclude.isEmpty()) {
-			return extractIncludedHeaders(headers, headersToInclude);
-		}
-
-		if (!headersToExclude.isEmpty()) {
-			return extractExcludedHeaders(headers, headersToExclude);
-		}
-
-		return headers;
-	}
-
-	/**
-	 * Filter sensitive headers from the given list of request headers.
-	 *
-	 * @param headers              The list of headers to filter.
-	 * @param maskSensitiveHeaders Whether to mask sensitive headers or not.
-	 * @return A map containing filtered headers.
-	 */
-	public static Map<String, String> filterSensitiveHeaders(Map<String, String> headers,
-			boolean maskSensitiveHeaders) {
-		if (maskSensitiveHeaders) {
-			Map<String, String> filteredHeaders = new HashMap<>();
-			for (Map.Entry<String, String> entry : headers.entrySet()) {
-				String key = entry.getKey().toLowerCase();
-				if (sensitiveHeaders.contains(key)) {
-					filteredHeaders.put(entry.getKey(), "**Redacted**");
-				} else {
-					filteredHeaders.put(entry.getKey(), entry.getValue());
-				}
-			}
-			return filteredHeaders;
-		}
-
-		return headers;
-	}
-
-	/**
-	 * Extracts headers to log based on inclusion criteria.
-	 * 
-	 * @param headers          The map of headers.
-	 * @param headersToInclude The set of headers to include.
-	 * @return The extracted headers to log.
-	 */
-	private Map<String, String> extractIncludedHeaders(Map<String, String> headers, List<String> headersToInclude) {
-		Map<String, String> extractedHeaders = new HashMap<>();
-		for (Map.Entry<String, String> entry : headers.entrySet()) {
-			if (headersToInclude.contains(entry.getKey().toLowerCase())) {
-				extractedHeaders.put(entry.getKey(), entry.getValue());
-			}
-		}
-		return extractedHeaders;
-	}
-
-	/**
-	 * Extracts headers to log based on exclusion criteria.
-	 * 
-	 * @param headers          The map of headers.
-	 * @param headersToExclude The set of headers to exclude.
-	 * @return The extracted headers to log.
-	 */
-	private Map<String, String> extractExcludedHeaders(Map<String, String> headers, List<String> headersToExclude) {
-		Map<String, String> extractedHeaders = new HashMap<>();
-		for (Map.Entry<String, String> entry : headers.entrySet()) {
-			if (!headersToExclude.contains(entry.getKey().toLowerCase())) {
-				extractedHeaders.put(entry.getKey(), entry.getValue());
-			}
-		}
-		return extractedHeaders;
 	}
 }
---
+++ src/test/java/apimatic/core/logger/SdkLoggerTest.java
@@ -0,0 +1,118 @@
+package apimatic.core.logger;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.event.Level;
+
+import io.apimatic.core.logger.SdkLogger;
+import io.apimatic.coreinterfaces.http.HttpHeaders;
+import io.apimatic.coreinterfaces.http.Method;
+import io.apimatic.coreinterfaces.http.request.Request;
+import io.apimatic.coreinterfaces.logger.Logger;
+import io.apimatic.coreinterfaces.logger.configuration.ReadonlyLogging;
+import io.apimatic.coreinterfaces.logger.configuration.ReadonlyRequestLogging;
+
+public class SdkLoggerTest {
+	/**
+	 * An instance of {@link ReadonlyLogging}.
+	 */
+	private ReadonlyLogging config;
+	
+	/**
+	 * An instance of {@link SdkLogger}.
+	 */
+	private SdkLogger sdkLogger;
+	
+	/**
+	 * An instance of {@link Logger}.
+	 */
+	private Logger logger;
+
+	@Before
+	public void setUp() {
+		config = mock(ReadonlyLogging.class);
+		logger = mock(Logger.class);
+		when(config.getLogger()).thenReturn(logger);
+		when(config.getLevel()).thenReturn(Level.INFO);
+		when(config.getRequestLogOptions()).thenReturn(mock(ReadonlyRequestLogging.class));
+		sdkLogger = new SdkLogger(config);
+	}
+
+	@Test
+	public void testLogRequest() {
+		Request request = mock(Request.class);
+		when(request.getUrl()).thenReturn("http://example.com");
+		when(request.getQueryUrl()).thenReturn("http://example.com");
+		when(request.getHttpMethod()).thenReturn(Method.GET);
+		HttpHeaders headers = mock(HttpHeaders.class);
+		when(headers.value("content-type")).thenReturn(null);
+		when(request.getHeaders()).thenReturn(headers);
+
+		sdkLogger.logRequest(request);
+
+		// Verify the log message with query parameters
+		verify(logger).log(eq(Level.INFO), eq("Request {} {} {}"), eq(Method.GET),
+				eq("http://example.com"), eq(""));
+	}
+	
+	@Test
+	public void testLogPostRequest() {
+		Request request = mock(Request.class);
+		when(request.getUrl()).thenReturn("http://example.com");
+		when(request.getQueryUrl()).thenReturn("http://example.com");
+		when(request.getHttpMethod()).thenReturn(Method.POST);
+		HttpHeaders headers = mock(HttpHeaders.class);
+		when(headers.value("content-type")).thenReturn("application/json");
+		when(request.getHeaders()).thenReturn(headers);
+
+		sdkLogger.logRequest(request);
+
+		// Verify the log message with query parameters
+		verify(logger).log(eq(Level.INFO), eq("Request {} {} {}"), eq(Method.POST),
+				eq("http://example.com"), eq("application/json"));
+	}
+	
+	@Test
+	public void testLogRequestWithBody() {
+		when(config.getRequestLogOptions().shouldLogBody()).thenReturn(true);
+		Request request = mock(Request.class);
+		when(request.getUrl()).thenReturn("http://example.com");
+		when(request.getQueryUrl()).thenReturn("http://example.com");
+		when(request.getHttpMethod()).thenReturn(Method.POST);
+		HttpHeaders headers = mock(HttpHeaders.class);
+		when(headers.value("content-type")).thenReturn(null);
+		when(request.getHeaders()).thenReturn(headers);
+		when(request.getBody()).thenReturn("Test request body");
+
+		sdkLogger.logRequest(request);
+
+		// Verify the log message with query parameters
+		verify(logger).log(eq(Level.INFO), eq("Request {} {} {}"), eq(Method.POST),
+				eq("http://example.com"), eq(""));
+		verify(logger).log(eq(Level.INFO), eq("Request Body {}"), eq("Test request body"));
+	}
+	
+	@Test
+	public void testLogRequestWithQueryParameters() {
+		when(config.getRequestLogOptions().shouldIncludeQueryInPath()).thenReturn(true);
+		
+		Request request = mock(Request.class);
+		when(request.getUrl()).thenReturn("http://example.com");
+		when(request.getQueryUrl()).thenReturn("http://example.com?param=value");
+		when(request.getHttpMethod()).thenReturn(Method.GET);
+		HttpHeaders headers = mock(HttpHeaders.class);
+		when(headers.value("content-type")).thenReturn(null);
+		when(request.getHeaders()).thenReturn(headers);
+
+		sdkLogger.logRequest(request);
+
+		// Verify the log message with query parameters
+		verify(logger).log(eq(Level.INFO), eq("Request {} {} {} queryParameters: {}"), eq(Method.GET),
+				eq("http://example.com"), eq(""), eq("param=value"));
+	}
+}
